a47fffbda1bd68c2180510a8f0173f93cb06b884
s1F10220209 <s1f102202099@iniad.org>
2024-08-26 14:56:54 +0900

    ログイン失敗の場合の機能

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,21 +1,20 @@
-# src/accounts/views.py
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
-from django.http import HttpResponse
 
 def dashboard(request):
     return render(request, 'accounts/dashboard.html')
 
 def login_signup(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
+        username = request.POST.get('username')
+        password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
             return redirect('privacy_policy')
         else:
-            return HttpResponse("ログインに失敗しました。")
+            # ログイン失敗時にエラーメッセージを渡す
+            return render(request, 'accounts/login_signup.html', {'error_message': 'ログインに失敗しました。'})
     return render(request, 'accounts/login_signup.html')
 
 def privacy_policy(request):
