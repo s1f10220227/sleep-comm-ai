ceb779bcdb48d83d7673260ff17b44cd63599ecc
s1F10220209 <s1f102202099@iniad.org>
2024-08-26 14:26:53 +0900

    adminに入れるように更新

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('dashboard/', views.dashboard, name='dashboard'),
     path('login/', views.login_signup, name='login_signup'),
     path('privacy_policy/', views.privacy_policy, name='privacy_policy'),
     path('profile/', views.profile, name='profile'),
