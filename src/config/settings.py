e7be6797fe48deb0c2ff0f63fac7333b4e6185a1
Masaki <s1f102202279@iniad.org>
2024-08-26 16:15:03 +0900

    modify_settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -28,13 +28,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-# 追加
-CSRF_TRUSTED_ORIGINS = [
-    'http://127.0.0.1:8080',
-    'http://localhost:8080',
-]
-
-
 # Application definition
 
 INSTALLED_APPS = [
