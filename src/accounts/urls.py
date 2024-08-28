347b1771428ae81a701e2af48491978d3993871d
s1F10220209 <s1f102202099@iniad.org>
2024-08-29 00:17:11 +0900

    signup機能を実装

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('login/', views.login_signup, name='login_signup'),
     path('privacy_policy/', views.privacy_policy, name='privacy_policy'),
     path('profile/', views.profile, name='profile'),
+    path('signup/', views.signup, name='signup'),  # サインアップ用のURLを追加
 ]
--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 
+#追加
+from django.contrib.auth.models import User
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        password_confirm = request.POST.get('password_confirm')
+
+        if password == password_confirm:
+            # ユーザーを作成
+            user = User.objects.create_user(username=username, password=password)
+            user.save()
+            return redirect('login_signup')  # サインアップ成功後にログインページへリダイレクト
+        else:
+            return render(request, 'accounts/signup.html', {'error_message': 'パスワードが一致しません。'})
+    return render(request, 'accounts/signup.html')
+
+
 def dashboard(request):
     return render(request, 'accounts/dashboard.html')
 
