6df681c8e1f16416d339ce47983a04a1a35e92b5
1F10220252 <s1f102202527@iniad.org>
2024-09-09 15:19:15 +0900

    Revert to the environment before adding MongoDB

--- src/config/settings.py
+++ src/config/settings.py
@@ -139,16 +139,5 @@ DATABASES = {
         'PASSWORD': 'password',
         'HOST': 'db',
         'PORT': 5432,
-    },
-    'mongo': {
-        'ENGINE': 'djongo',
-        'NAME': 'testdb',
-        'ENFORCE_SCHEMA': False,
-        'CLIENT': {
-            'host': 'mongo',
-            'port': 27017,
-            'username': 'iniad',
-            'password': 'password',
-        }
     }
 }
