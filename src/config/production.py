3c1033a1f584120edde5fd60a2104e6d7f0f0d9e
s1f10220227 <s1f102202279@iniad.org>
2024-12-04 19:22:58 +0900

    change

--- src/config/production.py
+++ src/config/production.py
@@ -2,12 +2,17 @@ import os
 import dj_database_url
 from .settings import *
 
-ALLOWED_HOSTS.append(os.environ['RENDER_EXTERNAL_HOSTNAME'])
+# ALLOWED_HOSTS に Render ドメインを追加
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
+# WhiteNoise ミドルウェア
 MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 
+# データベース設定 (DATABASE_URL を利用)
 DATABASES = {
-    'default': dj_database_url.config()
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
+# 静的ファイルの設定
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
