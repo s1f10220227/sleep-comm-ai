8a333b7cb497e62b099b469f107340f575465941
s1f10220227 <s1f102202279@iniad.org>
2024-12-04 20:57:43 +0900

    fix

--- src/config/production.py
+++ src/config/production.py
@@ -1,18 +1,9 @@
 import os
-import dj_database_url
+# import dj_database_url
 from .settings import *
 
 # ALLOWED_HOSTSの設定
 ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME', '')]
-
-# データベース設定
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,
-        conn_health_checks=True,
-    )
-}
 
 # その他の設定は同じ
 MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
