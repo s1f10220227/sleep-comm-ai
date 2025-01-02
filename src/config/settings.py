454fa41105759f4ddcdc26c2ca9df237f23cff01
1F10220023 <s1f102200233@iniad.org>
2025-01-02 22:40:04 +0900

    Update DEBUG configuration to use env.bool with default value

--- src/config/settings.py
+++ src/config/settings.py
@@ -33,7 +33,7 @@ OPENAI_API_BASE = env('OPENAI_API_BASE')
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = env.bool('DEBUG', default=False)
 
 ALLOWED_HOSTS = [
     'som-net.net',
