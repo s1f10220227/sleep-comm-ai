1281302d2a42bf47c8b9212ba8538df392642fbf
1F10220023 <s1f102200233@iniad.org>
2025-01-08 23:22:07 +0900

    Add export functionality for SleepAdvice model using django-import-export

--- src/chat/admin.py
+++ src/chat/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ExportMixin
+from import_export.formats import base_formats
+
 from .models import SleepAdvice, Message, Mission
 
-admin.site.register(SleepAdvice)
 admin.site.register(Message)
 admin.site.register(Mission)
+
+class SleepAdviceResource(resources.ModelResource):
+    # Modelに対するdjango-import-exportの設定
+    class Meta:
+        model = SleepAdvice
+
+@admin.register(SleepAdvice)
+class SleepAdviceAdmin(ExportMixin, admin.ModelAdmin):
+    resource_class = SleepAdviceResource
+    formats = [base_formats.CSV]
