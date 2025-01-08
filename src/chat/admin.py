3d2b5063f01d0b364616639afe19202e7ccc49ab
1F10220023 <s1f102200233@iniad.org>
2025-01-08 23:00:11 +0900

    Add Group, GroupMember, Message, and Mission models to admin panel

--- src/chat/admin.py
+++ src/chat/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import SleepAdvice
+from .models import SleepAdvice, Message, Mission
 
 admin.site.register(SleepAdvice)
+admin.site.register(Message)
+admin.site.register(Mission)
